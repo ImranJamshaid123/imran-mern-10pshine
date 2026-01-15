2a9bfaffe2f07dcae03dfbc8c1ea9c131852d26e
Imran Jamshaid <imranjamshaid534@gmail.com>
2026-01-15 11:30:39 -0800

    add notes CRUD controllers and add JWT-protected notes routes

--- backend/src/routes/note.Route.js
+++ backend/src/routes/note.Route.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import { authenticate } from '../middleware/auth.middleware.js';
+import {
+  createNote,
+  getNotes,
+  getNoteById,
+  updateNote,
+  deleteNote,
+} from '../controllers/note.controller.js';
+
 const router = express.Router();
 
 // All note routes are protected
 router.use(authenticate);
 
-router.get('/', (req, res) => {
-  res.json({ message: 'Get all notes', user: req.user });
-});
-
-router.post('/', (req, res) => {
-  res.json({ message: 'Create note', user: req.user });
-});
+router.post('/', createNote);
+router.get('/', getNotes);
+router.get('/:id', getNoteById);
+router.put('/:id', updateNote);
+router.delete('/:id', deleteNote);
 
 export default router;
